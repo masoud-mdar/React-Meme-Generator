ca944a151eb80a3e3b237e2566258b7d4d165830
Masoud Mohammadi Darvishvand <54075973+masoud-mdar@users.noreply.github.com>
2021-03-04 11:29:53 +0100

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+            <div className="foot-wrapper">
+                <div className="nav-title">
+                    <h5>Masoud Mohammadi Darvishvand</h5>                    
+                </div>
+                <div className="leaf-wrapper">
+                    <div className="leaf one"></div>
+                    <div className="leaf two"></div>
+                    <div className="leaf three"></div>
+                </div>
+                <div className="nav-link-wrapper">
+                    <a href="#" target="_blank">Github</a>
+                    <a href="#" target="_blank">Contact</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
